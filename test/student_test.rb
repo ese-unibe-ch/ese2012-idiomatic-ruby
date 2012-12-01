6b21d4297c8fc8350af44f23c0e9ac6eaba3c72f
Michael Kohler <michaelkohler@live.com>
2012-12-01 12:12:26 +0100

    Made the test runnable when clicking "All tests" in RubyMine and when clicking the test itself (otherwise it just works when you select the test itself) Added a new test to see if the average calculation is incorrect (otherwise you don't know if the description itself fails or the average calculation)
    modified:   test/student_test.rb

--- test/student_test.rb
+++ test/student_test.rb
@@ -1,20 +1,33 @@
       require 'test/unit'
-      require 'app/university/student'
+
+      def relative path
+        File.join(File.dirname(__FILE__), path)
+      end
+      require relative('../app/university/student')
 
       # syntax for inheritance
       class StudentTest < Test::Unit::TestCase
 
+        def setup
+          @student = University::Student.named('John')
+        end
+
         def test_award
-          student = University::Student.named( 'John')
-          student.add(6)
-          assert( student.award?, 'Student deserves an award')
+          @student.add(6)
+          assert(@student.award?, 'Student deserves an award')
         end
 
         def test_description
-          student = University::Student.named( 'John')
-          student.add(6)
-          assert( student.to_s.include?('John'), 'Student name is incorrect')
-          assert( student.to_s.include?('6'), 'Student average is incorrect')
+          @student.add(6)
+          assert(@student.to_s.include?('John'), 'Student name is incorrect')
+          assert(@student.to_s.include?('6'), 'Student average is incorrect')
+        end
+
+        def test_average
+          @student.add(3)
+          @student.add(5)
+          assert(@student.average.to_s.include?('4'), 'Student average is incorrectly calculated')
         end
 
       end
+
